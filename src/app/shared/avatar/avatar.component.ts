a85db205234d2be681fbe23385b5b7b7af9f22bb
Nicoleta Panaghiu <idlnicoleta@gmail.com>
2021-08-17 22:26:27 +0300

    Added a default avatar

--- src/app/shared/avatar/avatar.component.ts
+++ src/app/shared/avatar/avatar.component.ts
@@ -19,6 +19,8 @@ export class AvatarComponent implements OnInit {
   @Input()
   alt: string | undefined;
 
+  defaultAvatar = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.studnet.com.co%2Fwp-content%2Fuploads%2F2016%2F10%2Fdefault-avatar-1024x1024.png&f=1&nofb=1';
+
   constructor() { }
 
   ngOnInit(): void {
